3f2e71b3308ecb7f600b75f2fe20f89f3dc92985
CaioSeixasDuarte <caio.seixasduarte@hotmail.com>
2022-11-11 18:11:54 -0300

    teste commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using DotNET.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10,30);
+calc.Subtrair(10,50);
+calc.Multiplicar(10,20);
+calc.Dividir(81,9);
+calc.Potencia(2,5);
+calc.Seno(30);
+calc.Cosseno(30);
+calc.Tangente(30);
+calc.raizQuadrada(121);
+
+
+/*
+Console.WriteLine("Digite uma letra:");
+string letra = Console.ReadLine();
+*/
+/*
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+*/
+
+/*string apresentacao = "Olá!";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("A quantidade é: " + quantidade);
+Console.WriteLine("A altura é: " + altura);
+Console.WriteLine("O preço é: " + preco);
+Console.WriteLine("A condição é: " + condicao);
+*/
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Caio";
+//pessoa1.Idade = 25;
+//pessoa1.Apresentar();
